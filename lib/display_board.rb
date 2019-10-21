108a0a8a0c3a0e8fa665056c59b4cbb9b343bde2
Andy L <andyqly@hotmail.com>
2019-10-21 15:09:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board(board)
+def display_board(board + X)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -9,4 +9,4 @@ def display_board(board)
 
 end
 
-display_board(board + "X")
+display_board(board)
